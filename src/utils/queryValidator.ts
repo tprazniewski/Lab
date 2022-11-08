f3aec0d286ed96d948384510713e335295910ab9
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-08 02:45:03 +0100

    update

--- src/utils/queryValidator.ts
+++ src/utils/queryValidator.ts
@@ -12,12 +12,12 @@ export const queryValidator: RequestHandler = (req, res, next) => {
   const { date, samplekind, patientId } = req.query;
   const id = Number(patientId);
   if ((samplekind && samplekind !== bloodTest) || samplekind !== covidTest) {
-    res.status(404).send({
+    return res.status(404).send({
       message: `The kind of a sample doesn't match the requirements u migh have a white spaces or dont use the upper case letters`,
     });
   }
   if (id !== NaN) {
-    res.status(404).send({
+    return res.status(404).send({
       message: `The patientId must be a valid number`,
     });
   }
